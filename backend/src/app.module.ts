c2e4c70e14316fc03ec5426baca9f0cb3f8d9abf
thiagohenriquerossato <thiago.rossato.tr@gmail.com>
2023-01-05 19:04:19 -0300

    chore: cria auth controller, service e module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientModule } from './client/client.module';
+import { AuthService } from './auth/auth.service';
 
 @Module({
   imports: [ClientModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
